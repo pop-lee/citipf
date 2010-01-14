1a8a0de51eb43a8a5927bc3df4cc92f3b7b48282
flex.liyunpeng <flex.liyunpeng@e4a6f6e6-ffed-11de-a6f0-ed93ac2c91d0>
2010-01-14 10:46:51 +0000

    初始化项目导入文件

---
+++ trunk/CitiPF_RateCore/src/com/citipf/liyunpeng/PushRateStop.java
@@ -0,0 +1,7 @@
+package com.citipf.liyunpeng;
+
+public class PushRateStop {
+	public static void main(String[] args) {
+		PushDataService.getInstance().stop();
+	}
+}
---
+++ trunk/CitiPF_RateCore/src/com/citipf/liyunpeng/inte/IPushDataThread.java
@@ -0,0 +1,8 @@
+package com.citipf.liyunpeng.inte;
+
+public interface IPushDataThread extends Runnable {
+
+	public void setIsRunning(boolean _b) ;
+	
+	public boolean getIsRunning() ;
+}
