53d8710564b3c035cd7d24d650080aec5ef6f9ae
flex.liyunpeng <flex.liyunpeng@e4a6f6e6-ffed-11de-a6f0-ed93ac2c91d0>
2010-04-09 10:29:17 +0000

    Spring 3.0 IBatis 3.0集成 以及Dao配置

---
+++ trunk/CitiPF/src/com/citipf/liyunpeng/dao/iBatis/IBatisDaoSupport.java
@@ -0,0 +1,7 @@
+package com.citipf.liyunpeng.dao.iBatis;
+
+import org.springframework.orm.ibatis.support.SqlMapClientDaoSupport;
+
+public class IBatisDaoSupport extends SqlMapClientDaoSupport {
+
+}
